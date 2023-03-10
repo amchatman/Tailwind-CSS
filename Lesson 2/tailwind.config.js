a02958b4301e95057876a03a37c599b8b204fb66
Ashley Chatman <ashchat2008@gmail.com>
2023-03-10 16:56:59 -0500

    Mobile menu

--- Lesson 2/tailwind.config.js
+++ Lesson 2/tailwind.config.js
@@ -3,11 +3,6 @@
 module.exports = {
   content: ["./build/*html"],
   theme: {
-    colors:{
-      light: '#67e8f9',
-      DEFAULT: '#06b6d4',
-      dark: '#0e7490',
-    },
     extend: {},
   },
   plugins: [],
