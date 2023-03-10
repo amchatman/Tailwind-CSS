d1a9fefa874d9febf577c6449f5329787f41c8fb
Ashley Chatman <ashchat2008@gmail.com>
2023-03-10 16:10:24 -0500

    Color Update

--- Lesson 2/tailwind.config.js
+++ Lesson 2/tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./build/*html"],
   theme: {
+    colors:{
+      light: '#67e8f9',
+      DEFAULT: '#06b6d4',
+      dark: '#0e7490',
+    },
     extend: {},
   },
   plugins: [],
